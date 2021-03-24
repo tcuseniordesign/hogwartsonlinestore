b08f3f7cf21e6e4d02fde17c2569a464b9e88ac8
Washingtonwei <weibingyang@gmail.com>
2021-03-24 16:35:37 -0500

    Initial commit with static html pages

---
+++ src/main/java/edu/tcu/cs/hogwartsonlinestore/HogwartsonlinestoreApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartsonlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsonlinestoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsonlinestoreApplication.class, args);
+    }
+
+}
